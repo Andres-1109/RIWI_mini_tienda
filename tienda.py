1df110860c38ff91fde4402ea00359b7685b04df
Andres-1109 <felipe11092002@gmail.com>
2026-03-02 18:11:54 -0500

    Type of client and new discounts

--- tienda.py
+++ tienda.py
@@ -1,18 +1,53 @@
 
 import os
+print("Types of clients:")
+print("0. Irregular")
+print("1. Regular")
+print("2. VIP")
+clientType = 0
+while True:
+    try:
+        clientType = int(input("Insert the type of client:"))
+        if 0<=clientType<=2:
+            break 
+        else:
+            print("Insert a number between 0 and 2")   
+    except ValueError:
+        print("Insert a valid number")
+    
+        print("Insert a number between 0 and 2")
 
+while True:        
+    name = str(input("Please insert the name of the client: "))
+    if name == None:
+        print("Name is required")
+    else:
+        break
 
-name = str(input("Please insert the name of the client: "))
-product = str(input("Please insert the name of the product: "))     
+while True:        
+    product = str(input("Please insert the name of the product: "))     
+    if product == None:
+        print("Product name is required")
+    else:
+        break
+
 while True:
     try:
         price = float(input("Please insert the price of the product: "))
-        break
+        if price <= 0:
+            print("Please insert a valid price")
+        else:
+            break
     except ValueError:
-        print("Please insert a valid price")        
+        print("Please insert a valid price")    
+
 while True:
     try:
         quantity = float(input("Please insert the quantity of the prouct: "))
+        if quantity <= 0:
+            print("Please insert a valid quantity")
+        else:
+            break
         break
     except ValueError:
         print("Please insert a valid quantity")        
@@ -29,9 +64,36 @@ if subtotal>= 50000:
 else:
     totalgross = subtotal
 
+#ClientDiscount
+nameClientType = None
+if clientType == 0:
+    clientDiscount = totalgross* 0
+    nameClientType = "Irregular"
+elif clientType == 1:
+    clientDiscount = totalgross * 0.05
+    nameClientType = "Regular"
+elif clientType == 2 and subtotal > 200000:
+    clientDiscount = totalgross * 0.15
+    nameClientType ="VIP"
+else:
+    clientDiscount = totalgross * 0.1
+    nameClientType = "VIP"
+
+totalgross -= clientDiscount 
+
 #Taxes calculation    
 iva = totalgross*0.19
 totalnet = totalgross + iva
+
+# Type of purchase
+typeOfPurchase = None
+if totalnet < 50000:
+    typeOfPurchase = "Small"
+elif 50000<=totalnet<=150000:
+    typeOfPurchase = "Medium"
+else:
+    typeOfPurchase = "Big"
+
 
 os.system("clear")
 print(f"The name of the client is: {name}")
@@ -47,3 +109,9 @@ print("==========")
 print(f"The IVA is: {iva}")
 print("==========")
 print(f"The total net is: {totalnet}")
+print("==========")
+print(f"Thanks for your purchase {name}")
+print("==========")
+print(f"You are a {nameClientType} client")
+print("==========")
+print(f"Your purchase was {typeOfPurchase}")
