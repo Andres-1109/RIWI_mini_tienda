b82eabe4836b6e49b8acb96dfad7bc9ea17318b7
Andres-1109 <felipe11092002@gmail.com>
2026-03-03 18:50:56 -0500

    Completed all the needed entries

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 window= tk.Tk()
 window.title("Mini tienda")
-window.geometry("800x800")
+window.geometry("1200x1200")
 
     # Type of client section
 lbTypeOfClients= tk.Label(window, text="Types of clients\n0. Irregular\n1. Regular\n2. VIP")
@@ -13,10 +13,11 @@ askClientType.pack()
 
 messageTypeOfclient = tk.Label(window, text="")
 messageTypeOfclient.pack()
+clientType = askClientType.get()
 
 def validateTypeOfClients():
     try:
-        clientType = int(askClientType.get())
+        clientType = int(clientType)
         if 0<=clientType<=2: 
             messageTypeOfclient.config(text="Valid client type")
             nameSection.pack()
@@ -37,13 +38,13 @@ askname = tk.Entry(nameSection)
 askname.pack()
 messageName = tk.Label(nameSection, text="")
 messageName.pack()
+name = askname.get()
 
 def validateName():
-    name = askname.get()
     if name == "":
         messageName.config(text="Name is required")
     else:
-        messageName.config(text="Name is valid")
+        messageName.config(text="The name is valid")
         productSection.pack()
     
 buttonName = tk.Button(nameSection, text="Continue", command=validateName)
@@ -57,16 +58,65 @@ askProduct = tk.Entry(productSection)
 askProduct.pack()
 messageProduct = tk.Label(productSection, text="")
 messageProduct.pack()
+product = askProduct.get()
 
 def validateProduct():
-    product = askProduct.get()
     if product == "":
         messageProduct.config(text="Product name is required")
     else:
-        messageProduct.config(text="Product name is valid")
+        messageProduct.config(text="The product name is valid")
+        priceSection.pack()
     
 buttonProduct = tk.Button(productSection, text="Continue", command=validateProduct)
 buttonProduct.pack()
+
+    #Price Section
+priceSection= tk.Frame(window)
+LabelPrice = tk.Label(priceSection, text="Please insert the price of the product: ")
+LabelPrice.pack()
+askPrice = tk.Entry(priceSection)
+askPrice.pack()
+messagePrice= tk.Label(priceSection, text="")
+messagePrice.pack()
+price = askPrice.get()
+
+def validatePrice():
+    try:
+        price = float(price)
+        if price<= 0:
+            messagePrice.config(text="Please insert a valid price")
+        else:
+            messagePrice.config(text="The price is valid")
+            quantitySection.pack()
+    except ValueError:
+        messagePrice.config(text="Please insert a valid price")
+
+buttonPrice = tk.Button(priceSection, text="Continue", command=validatePrice)
+buttonPrice.pack()
+
+    #Quantity Section
+quantitySection = tk.Frame(window)
+labelQuantity = tk.Label(quantitySection, text="Please insert the quantity of the prouct: ")
+labelQuantity.pack()
+askQuantity = tk.Entry(quantitySection)
+askQuantity.pack()
+messageQuantity = tk.Label(quantitySection, text="")
+messageQuantity.pack()
+quantity = askQuantity.get()
+
+def validateQuantity():
+    try:
+        quantity = float(quantity)
+        if quantity<= 0:
+            messageQuantity.config(text="Please insert a valid quantity")
+        else:
+            messageQuantity.config(text="The quantity is valid")
+    except ValueError:
+        messageQuantity.config(text="Please insert a valid quantity")
+
+buttonQuantity = tk.Button(quantitySection, text="Continue", command=validateQuantity)
+buttonQuantity.pack()
+
 
 
 window.mainloop()
